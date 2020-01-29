2a5de9e98eb6d850b76f9d1e15759781df85b0fb
Ramazan Yilmaz <R.Yilmaz@noviuniversity.com>
2020-01-29 14:20:38 +0100

    loginController

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,8 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello Ramazan Yilmaz");
+        logingController controller = new logingController();
+        controller.login();
+        System.out.println("hello Ramazan");
     }
 }
